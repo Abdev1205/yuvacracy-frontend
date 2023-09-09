5d8ef244edf0f26a8fed0e40e745d424428725e2
Abhay1205Mishra <abhaym1205@gmail.com>
2023-09-09 22:59:51 +0530

    added about us section

--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,12 @@
 import Navbar from "@/components/Navbar"
 import Hero from "@/components/home/Hero"
-
+import About from "@/components/home/About"
 const home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
       <div className=" h-[60rem] ">
 
       </div>
