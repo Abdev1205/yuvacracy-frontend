f679088b843a5983b231289ad3829ac423b4adf5
Abhay1205Mishra <abhaym1205@gmail.com>
2023-09-11 12:54:17 +0530

    adjusted contact  section background for smaller device

--- components/home/Contact.js
+++ components/home/Contact.js
@@ -1,9 +1,10 @@
 import Image from "next/image"
 import SecondaryButton from "../SecondaryButton"
+import PrimaryButton from "../PrimaryButton"
 const Contact = () => {
   return (
     <>
-      <div className=" select-none mx-auto flex sm:justify-evenly xl_phone:w-[80%] w-[100%] h-[auto] py-[2rem] sm:px-[1.5rem] xl_phone:rounded-[1rem] contact-us-gradient " >
+      <div className=" select-none mx-auto flex sm:justify-evenly xl_phone:w-[80%] w-[100%] h-[auto] py-[2rem] sm:px-[1.5rem] xl_phone:rounded-[1rem] contact-us-gradient    " >
         <div className=" flex flex-col justify-center sm:items-start  mx-auto sm:w-[38%] md_phone:w-[22rem] w-[90%] gap-[1rem] " >
           <h2 className="text-white font-montserrat font-[700] lg_phone:text-[2.5rem] text-[2rem]" >Contact us</h2>
           <Image
@@ -13,7 +14,7 @@ const Contact = () => {
           />
           <p className=" text-[#D8D5D5] text-[.9rem] font-openSans font-[400] mb-[1rem]  " >Do you have inquiries or wish to learn more about YuvaCracy? Please do not hesitate to contact us. Please get in touch with us.</p>
           <div>
-            <SecondaryButton data={"CONTACT US"} />
+            <PrimaryButton data={"Contact Us"} />
           </div>
         </div>
         <div className=" sm:flex hidden justify-center w-[40%]  " >
